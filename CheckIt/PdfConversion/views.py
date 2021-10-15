add90bb71b9654e568af53dd76e763679a12a858
Sananda <sananda@iut-dhaka.edu>
2021-10-15 21:56:04 +0600

    added spinner in pdf conversion file

--- CheckIt/PdfConversion/views.py
+++ CheckIt/PdfConversion/views.py
@@ -196,7 +196,7 @@ def ConvertPdf(request,pdf):
             image_counter = 1
 
             for page in pages:
-                filename = "page_"+str(pdf)+str(image_counter)+".jpg"
+                filename = "page_"+str(file_name)+str(image_counter)+".jpg"
                 page.save(filename, 'JPEG')
                 image_counter = image_counter + 1
 
@@ -206,7 +206,7 @@ def ConvertPdf(request,pdf):
             f = open(outfile, "a")
 
             for i in range(1, filelimit + 1):
-                filename = "page_"+str(pdf)+str(i)+".jpg"
+                filename = "page_"+str(file_name)+str(i)+".jpg"
                 img = cv2.imread(filename)
 
                 # 2. Resize the image
@@ -223,7 +223,7 @@ def ConvertPdf(request,pdf):
             f.close()
 
             for i in range(1, filelimit + 1):
-                filename = "page_"+str(pdf)+str(i)+".jpg"
+                filename = "page_"+str(file_name)+str(i)+".jpg"
                 original = 'C:/Users/ASUS/Desktop/DP/CheckIT/CheckIt/'+ filename
                 target = r'C:\Users\ASUS\Desktop\DP\CheckIT\CheckIt\media\convertedImg'
                 shutil.move(original,target)
